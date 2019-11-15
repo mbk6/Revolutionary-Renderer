326b21dc80e07f035a149da2784201a33ba117ea
Mike <mbk6@illinois.edu>
2019-11-15 13:39:36 -0600

    Added width and height constants and constructor to pass them in to the ofApp. Tested drawing of circles, lines, and rectangles.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,71 +1,78 @@
 #include "ofApp.h"
 
+Renderer::Renderer(int width, int height) {
+	win_width = width;
+    win_height = height;
+}
+
 //--------------------------------------------------------------
-void ofApp::setup(){
+void Renderer::setup(){
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void Renderer::update(){
+	
+}
+
+//--------------------------------------------------------------
+void Renderer::draw(){ 
+	ofDrawCircle(100, 100, 100);
+    ofDrawLine(50, 50, 600, 700);
+    ofDrawRectangle(ofVec2f(300, 350), 40, 200);
+}
+
+//--------------------------------------------------------------
+void Renderer::keyPressed(int key){
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void Renderer::keyReleased(int key){
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void Renderer::mouseMoved(int x, int y ){
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void Renderer::mouseDragged(int x, int y, int button){
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void Renderer::mousePressed(int x, int y, int button){
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void Renderer::mouseReleased(int x, int y, int button){
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void Renderer::mouseEntered(int x, int y){
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void Renderer::mouseExited(int x, int y){
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void Renderer::windowResized(int w, int h){
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void Renderer::gotMessage(ofMessage msg){
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void Renderer::dragEvent(ofDragInfo dragInfo){ 
 
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,23 +2,34 @@
 
 #include "ofMain.h"
 
-class ofApp : public ofBaseApp{
+#include <vector>
 
-	public:
-		void setup();
-		void update();
-		void draw();
+class Renderer : public ofBaseApp {
+   public:
+    // Constructor passes in window size
+    Renderer(int width, int height);
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+	// Window parameters
+    int win_width;
+    int win_height;
+
+    void setup();
+    void update();
+    void draw();
+
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y);
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+
+
+
+
 };
